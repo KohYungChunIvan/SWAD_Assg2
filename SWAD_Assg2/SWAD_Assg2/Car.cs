80ea4c3eb3dcb10ca655efb76d55c226db6ce38e
KohYungChunIvan <150001174+KohYungChunIvan@users.noreply.github.com>
2024-08-04 15:20:14 +0800

    new

---
+++ SWAD_Assg2/SWAD_Assg2/Car.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace swad_assg2
+{
+    public class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public int Mileage { get; set; }
+        public string Color { get; set; }
+        public string LicensePlate { get; set; }
+        public string VIN { get; set; }
+        public string Photo { get; set; }
+
+        public Car(string make, string model, int year, int mileage, string color, string licensePlate, string vin, string photo)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+            Mileage = mileage;
+            Color = color;
+            LicensePlate = licensePlate;
+            VIN = vin;
+            Photo = photo;
+        }
+    }
+
+}
---
+++ SWAD_Assg2/SWAD_Assg2/Renter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace swad_assg2
+{
+    public class Renter : User
+    {
+        public string RenterId { get; set; }
+        public string BookingHistory { get; set; }
+        public string PaymentDetails { get; set; }
+        public string VerificationStatus { get; set; }
+        public bool IsPrime { get; set; }
+        public string UpcomingRentals { get; set; }
+        public bool IsPenalised { get; set; }
+    }
+
+}
